26f35c4eea174bb3f2783aa56cdf08ac734d9459
youcanalsocode <nehan.gupta12@gmail.com>
2024-05-30 12:40:24 +0530

    Adding navbar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Navbar from "./Component/Navbar";
 
 function App() {
   return (
     <>
-      <div className="container">I am the start</div>
+      <Navbar></Navbar>
     </>
   );
 }
